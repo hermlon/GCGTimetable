2d41e1472727474002de0917d31a389da6cca803
hermlon <dev.hermlon@yamx.net>
2019-10-26 22:14:13 +0200

    Removed unnecessary example code

--- app/src/main/java/net/hermlon/gcgtimetable/MainActivity.kt
+++ app/src/main/java/net/hermlon/gcgtimetable/MainActivity.kt
@@ -24,11 +24,6 @@ class MainActivity : AppCompatActivity() {
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -36,8 +31,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send
+                R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/net/hermlon/gcgtimetable/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/net/hermlon/gcgtimetable/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package net.hermlon.gcgtimetable.ui.send
+package net.hermlon.gcgtimetable.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SendViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is send Fragment"
+        value = "This is settings Fragment"
     }
     val text: LiveData<String> = _text
 }
