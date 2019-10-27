88869136c37aa9f8dce8983f3c2fbd6e4615b3b4
hermlon <dev.hermlon@yamx.net>
2019-10-27 13:11:10 +0100

    Added DataBinding

--- app/src/main/java/net/hermlon/gcgtimetable/MainActivity.kt
+++ app/src/main/java/net/hermlon/gcgtimetable/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
