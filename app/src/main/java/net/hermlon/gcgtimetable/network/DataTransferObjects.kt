62cc913922a2d9fbbc93379192a6faec955d403b
hermlon <dev.hermlon@yamx.net>
2020-01-28 19:07:19 +0100

    Added some DataTransferObjects

--- app/src/main/java/net/hermlon/gcgtimetable/network/DataTransferObjects.kt
+++ app/src/main/java/net/hermlon/gcgtimetable/network/DataTransferObjects.kt
@@ -6,7 +6,9 @@ data class NetworkParseResult(
     val courses: List<NetworkCourse>,
     val lessons: List<NetworkLesson>,
     val day: NetworkDay,
-    val freeDays: List<Date>
+    val freeDays: List<Date>,
+    val exams: List<NetworkExam>,
+    val standardLessons: List<NetworkStandardLesson>
 )
 
 data class NetworkDay(
@@ -34,4 +36,21 @@ data class NetworkLesson(
     val roomChanged: Boolean = false,
     val courseId: Int,
     val information: String
+)
+
+data class NetworkExam(
+    val dayId: Int,
+    val number: Int,
+    val beginsAt: String,
+    val length: Int,
+    val information: String,
+    val courseId: Int
+)
+
+data class NetworkStandardLesson(
+    val dayOfWeek: Int,
+    val className: String,
+    val number: Int,
+    val courseId: Int,
+    val room: String
 )
