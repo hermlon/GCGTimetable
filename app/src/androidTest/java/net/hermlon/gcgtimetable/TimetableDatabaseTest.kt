55767db3837adfde254e1c43a9c08595960377d7
hermlon <dev.hermlon@yamx.net>
2020-02-09 19:03:55 +0100

    Modified DatabaseTest for lessons entity

--- app/src/androidTest/java/net/hermlon/gcgtimetable/TimetableDatabaseTest.kt
+++ app/src/androidTest/java/net/hermlon/gcgtimetable/TimetableDatabaseTest.kt
@@ -4,9 +4,15 @@ import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.hermlon.gcgtimetable.database.TimetableDatabase
+import net.hermlon.gcgtimetable.database.TimetableLessonDao
 import net.hermlon.gcgtimetable.database.TimetableProfile
 import net.hermlon.gcgtimetable.database.TimetableProfileDao
+import net.hermlon.gcgtimetable.network.NetworkLesson
+import net.hermlon.gcgtimetable.network.asDatabaseModel
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -16,7 +22,6 @@ import java.io.IOException
 @RunWith(AndroidJUnit4::class)
 class TimetableDatabaseTest {
 
-    private lateinit var timetableProfileDao: TimetableProfileDao
     private lateinit var db: TimetableDatabase
 
     @Before
@@ -28,7 +33,6 @@ class TimetableDatabaseTest {
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
             .build()
-        //timetableProfileDao = db.timetableProfileDao
     }
 
     @After
@@ -39,10 +43,16 @@ class TimetableDatabaseTest {
 
     @Test
     @Throws(Exception::class)
-    fun insertAndGetNight() {
-        var timetableProfile = TimetableProfile(profileName = "Test Profil")
-        timetableProfileDao.insert(timetableProfile)
-        val profile = timetableProfileDao.get(1)
-        assertEquals(profile?.profileName, "Test Profil")
+    fun insertAndGetLesson() {
+        GlobalScope.launch(Dispatchers.Main) {
+            db.lessonDao.insertAll(*setOf(
+                NetworkLesson("7/1", 1, "Deu", false, "Kipp", false, "207", false, 235, information = "")
+            ).asDatabaseModel(34))
+
+
+            db.lessonDao.getLessons().observeForever {
+                assertEquals(it[0].className, "7/1")
+            }
+        }
     }
 }
