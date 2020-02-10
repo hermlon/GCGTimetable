6b280bfe10b703e829b49380de4d7ee3a0a49e7d
hermlon <dev.hermlon@yamx.net>
2020-02-10 16:27:17 +0100

    cleaned up dependencies

--- app/src/androidTest/java/net/hermlon/gcgtimetable/Stundenplan24StudentXMLParserTest2.kt
+++ app/src/androidTest/java/net/hermlon/gcgtimetable/Stundenplan24StudentXMLParserTest2.kt
@@ -1,37 +1,38 @@
 package net.hermlon.gcgtimetable
 
 import android.util.Log
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import kotlinx.coroutines.*
-import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
 import net.hermlon.gcgtimetable.api.TimetableRepository
-import net.hermlon.gcgtimetable.database.*
+import net.hermlon.gcgtimetable.database.TimetableDatabase
+import net.hermlon.gcgtimetable.database.TimetableSource
+import net.hermlon.gcgtimetable.database.TimetableSourceDao
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import ru.gildor.coroutines.okhttp.await
 import java.io.IOException
 import java.text.SimpleDateFormat
-import java.util.*
 
 @RunWith(AndroidJUnit4::class)
 class Stundenplan24StudentXMLParserTest2 {
 
-    private lateinit var timetableSourceDao: TimetableSourceDao
-    private lateinit var db: TimetableDatabase
-    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private lateinit var database: TimetableDatabase
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun createDb() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         // Using an in-memory database because the information stored here disappears when the
         // process is killed.
-        db = Room.inMemoryDatabaseBuilder(context, TimetableDatabase::class.java)
+        database = Room.inMemoryDatabaseBuilder(context, TimetableDatabase::class.java)
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
             .build()
@@ -40,47 +41,31 @@ class Stundenplan24StudentXMLParserTest2 {
     @After
     @Throws(IOException::class)
     fun closeDb() {
-        db.close()
+        database.close()
     }
 
-    @Before
-    fun setUp() {
-        Dispatchers.setMain(mainThreadSurrogate)
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain() // reset main dispatcher to the original Main dispatcher
-        mainThreadSurrogate.close()
-    }
-
+    // runBlocking used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // TODO replace with runBlockingTest once issue is resolved
     @Test
-    @Throws(Exception::class)
-    fun fetchXML() {
+    fun fetchXML() = runBlocking {
         var source = TimetableSource(
             sourceName = "Test school",
             url = "https://www.stundenplan24.de/10000000/mobil")
-        var repo = TimetableRepository(db)
-        if (source != null) {
-            runBlocking {
-                var day = SimpleDateFormat("dd.MM.yyyy").parse("05.02.2020")
+        var repo = TimetableRepository(database)
+        Log.d("Test", "Dadadaaaa")
 
-                repo.fetch(source, day)
-            }
-            GlobalScope.launch(Dispatchers.Main) {
-                db.lessonDao.getLessons().observeForever { lessons ->
-                    Log.d("Test", "Lessons update!")
-                    Log.d("Test", lessons.toString())
-                    lessons.forEach {
-                        Log.d("Test", it.toString())
-                    }
-                }
-            }
+        Log.d("Test", "GlobalScope")
+        var day = SimpleDateFormat("dd.MM.yyyy").parse("05.02.2020")
 
-            assert(true)
-        }
-        else {
-            assert(false)
-        }
+        repo.fetch(source, day)
+        Log.d("Test", "Fetched stuff")
+        /*
+        database.lessonDao.getLessons().observeForever { lessons ->
+            Log.d("Test", "Lessons update!")
+            Log.d("Test", lessons.toString())
+            lessons.forEach {
+                Log.d("Test", it.toString())
+            }
+        }*/
     }
 }
--- app/src/androidTest/java/net/hermlon/gcgtimetable/TimetableDatabaseTest.kt
+++ app/src/androidTest/java/net/hermlon/gcgtimetable/TimetableDatabaseTest.kt
@@ -1,35 +1,48 @@
 package net.hermlon.gcgtimetable
 
+import android.util.Log
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
 import androidx.test.platform.app.InstrumentationRegistry
-import junit.framework.TestCase.assertEquals
+import androidx.test.runner.AndroidJUnit4
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import net.hermlon.gcgtimetable.api.TimetableRepository
+import net.hermlon.gcgtimetable.database.DatabaseLesson
 import net.hermlon.gcgtimetable.database.TimetableDatabase
-import net.hermlon.gcgtimetable.database.TimetableLessonDao
-import net.hermlon.gcgtimetable.database.TimetableProfile
-import net.hermlon.gcgtimetable.database.TimetableProfileDao
+import net.hermlon.gcgtimetable.database.TimetableSource
 import net.hermlon.gcgtimetable.network.NetworkLesson
 import net.hermlon.gcgtimetable.network.asDatabaseModel
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.notNullValue
 import org.junit.After
+import org.junit.Assert.assertThat
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.io.IOException
+import java.text.SimpleDateFormat
 
 @RunWith(AndroidJUnit4::class)
 class TimetableDatabaseTest {
 
-    private lateinit var db: TimetableDatabase
+    private lateinit var database: TimetableDatabase
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Before
     fun createDb() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
         // Using an in-memory database because the information stored here disappears when the
         // process is killed.
-        db = Room.inMemoryDatabaseBuilder(context, TimetableDatabase::class.java)
+        database = Room.inMemoryDatabaseBuilder(context, TimetableDatabase::class.java)
             // Allowing main thread queries, just for testing.
             .allowMainThreadQueries()
             .build()
@@ -38,21 +51,58 @@ class TimetableDatabaseTest {
     @After
     @Throws(IOException::class)
     fun closeDb() {
-        db.close()
+        database.close()
     }
 
     @Test
-    @Throws(Exception::class)
-    fun insertAndGetLesson() {
-        GlobalScope.launch(Dispatchers.Main) {
-            db.lessonDao.insertAll(*setOf(
-                NetworkLesson("7/1", 1, "Deu", false, "Kipp", false, "207", false, 235, null)
-            ).asDatabaseModel(34))
+    fun insertLessonAndGetById() = runBlockingTest {
+        // GIVEN - insert a task
+        val lesson = NetworkLesson("7/1", 1, "Deu", false, "Kipp", false, "207", false, 235, null)
+        val databaseLesson = setOf(lesson).asDatabaseModel(24)
+        database.lessonDao.insertAll(*databaseLesson)
 
-
-            db.lessonDao.getLessons().observeForever {
-                assertEquals(it[0].className, "7/1")
-            }
+        // WHEN - Get the task by id from the database.u
+        database.lessonDao.getLessons().observeForever {
+            val loaded = it[0]
+            // THEN - The loaded data contains the expected values
+            assertThat<DatabaseLesson>(loaded, notNullValue())
+            assertThat(loaded.className, `is`(lesson.className))
+            assertThat(loaded.courseId, `is` (lesson.courseId))
+            assertThat(loaded.dayId, `is` (24L))
+            assertThat(loaded.information, `is` (lesson.information))
+            assertThat(loaded.subject, `is` (lesson.subject))
+            assertThat(loaded.subjectChanged, `is` (lesson.subjectChanged))
+            assertThat(loaded.teacher, `is` (lesson.teacher))
+            assertThat(loaded.teacherChanged, `is` (lesson.teacherChanged))
+            assertThat(loaded.room, `is` (lesson.room))
+            assertThat(loaded.roomChanged, `is` (lesson.roomChanged))
         }
+    }
+
+    // runBlocking used here because of https://github.com/Kotlin/kotlinx.coroutines/issues/1204
+    // TODO replace with runBlockingTest once issue is resolved
+    @Test
+    fun fetchXML() = runBlocking {
+
+        /*
+        var source = TimetableSource(
+            sourceName = "Test school",
+            url = "https://www.stundenplan24.de/10000000/mobil")
+        var repo = TimetableRepository(database)
+        Log.d("Test", "Dadadaaaa")
+
+        Log.d("Test", "GlobalScope")
+        var day = SimpleDateFormat("dd.MM.yyyy").parse("05.02.2020")
+*/
+        //repo.fetch(source, day)
+        Log.d("Test", "Fetched stuff")
+        /*
+        database.lessonDao.getLessons().observeForever { lessons ->
+            Log.d("Test", "Lessons update!")
+            Log.d("Test", lessons.toString())
+            lessons.forEach {
+                Log.d("Test", it.toString())
+            }
+        }*/
     }
 }
